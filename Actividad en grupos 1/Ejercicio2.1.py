1f397f62f5138e65c0d9241f89e2b47019d04cbe
Daniel Osorio <dosorioos@unal.edu.co>
2026-02-28 16:45:43 -0500

    Agrego la parte adicional de los ejercicios 2.1 y 2.2

--- Actividad en grupos 1/Ejercicio2.1.py
+++ Actividad en grupos 1/Ejercicio2.1.py
@@ -1,27 +1,34 @@
 #Ejercicio 2.1 POO
 #Daniel Osorio - Isabella Caro - Laura Gomez
 
-
 class Persona: 
-    def __init__(self, nombre, apellido, id, año_de_nacimiento):
+    def __init__(self, nombre, apellido, id, año_de_nacimiento, pais_nacimiento, genero):
         self.nombre = nombre
         self.apellido = apellido
         self.id = id
         self.año_de_nacimiento = año_de_nacimiento
+        self.pais_nacimiento = pais_nacimiento
+        self.genero = genero 
 
     def ver_datos_persona(self):
         print("Datos de la persona: ")
         print(self.nombre, self.apellido)
         print(self.id)
         print(self.año_de_nacimiento)
+        print(self.año_de_nacimiento)
+        print(self.año_de_nacimiento)
+        print(self.pais_nacimiento)
+        print(self.genero)
         print()
 
 nombre = input("Ingrese el nombre de la persona: ")
 apellido = input("Ingrese el apellido de la persona: ")
 id = input("Ingrese el documento de identidad de la persona: ")
 año_de_nacimiento = int(input("Ingrese el año de nacimiento de la persona: "))
+pais_nacimiento =input("Ingrese el pais de nacimiento de la persona: ")
+genero = input("Ingrese el genero de la persona: ")
 
-nueva_persona = Persona(nombre, apellido, id, año_de_nacimiento)
+nueva_persona = Persona(nombre, apellido, id, año_de_nacimiento, pais_nacimiento, genero)
 nueva_persona.ver_datos_persona()
 
         
--- Actividad en grupos 1/Ejercicio2.2.py
+++ Actividad en grupos 1/Ejercicio2.2.py
@@ -9,7 +9,7 @@ class Tipo_de_planeta(Enum):
     Enano = "ENANO"
 
 class Planeta:
-    def __init__(self, nombre, cantidad_satelites, masa, volumen, diametro, distancia_media_sol, tipo, observabilidad):
+    def __init__(self, nombre, cantidad_satelites, masa, volumen, diametro, distancia_media_sol, tipo, observabilidad, periodo_orbital, periodo_rotacion):
         self.nombre = nombre
         self.cantidad_satelites = cantidad_satelites
         self.masa = masa
@@ -18,6 +18,8 @@ class Planeta:
         self.distancia_media_sol = distancia_media_sol
         self.tipo = tipo
         self.observabilidad = observabilidad
+        self.periodo_orbital = periodo_orbital
+        self.periodo_rotacion = periodo_rotacion
     
     def mostrar_datos_planeta(self):
         print("Nombre del planeta: ", self.nombre)
@@ -28,6 +30,8 @@ class Planeta:
         print("Distancia al sol: ", self.distancia_media_sol)
         print("Tipo de planeta: ", self.tipo.name)
         print("¿Es observable?: ", self.observabilidad)
+        print("Periodo orbital del planeta: ", self.periodo_orbital)
+        print("Periodo de rotación del planeta: ", self.periodo_rotacion)
 
     def densidad_planeta(self):
         if self.volumen == 0:
@@ -43,12 +47,14 @@ class Planeta:
             return False
         
 
-nombre_p1 = input("ingrese el nombre del planeta 1: ")
-cantidad_satelites_p1 = int(input("ingrese la cantidad de satelites del planeta 1: "))
-masa_p1 = float(input("ingrese la masa (en kg) del planeta 1: "))
-volumen_p1 = float(input("ingrese el volumen (en km^3) del planeta 1: "))
-diametro_p1 = int(input("ingrese el diametro (en km) del planeta 1: "))
-distancia_media_sol_p1 = int(input("ingrese la distancia media al sol (en km) del planeta 1: "))
+nombre_p1 = input("Ingrese el nombre del planeta 1: ")
+cantidad_satelites_p1 = int(input("Ingrese la cantidad de satelites del planeta 1: "))
+masa_p1 = float(input("Ingrese la masa (en kg) del planeta 1: "))
+volumen_p1 = float(input("Ingrese el volumen (en km^3) del planeta 1: "))
+diametro_p1 = int(input("Ingrese el diametro (en km) del planeta 1: "))
+distancia_media_sol_p1 = int(input("Ingrese la distancia media al sol (en km) del planeta 1: "))
+periodo_orbital_p1 = float(input("Ingrese el periodo orbital (en años) del planeta 1: "))
+periodo_rotacion_p1 = float(input("Ingrese el periodo de rotación (en dias) del planeta 1: "))
 
 nombre_p2 = input("ingrese el nombre del planeta 2: ")
 cantidad_satelites_p2 = int(input("ingrese la cantidad de satelites del planeta 2: "))
@@ -56,9 +62,11 @@ masa_p2 = float(input("ingrese la masa (en kg) del planeta 2: "))
 volumen_p2 = float(input("ingrese el volumen (en km^3) del planeta 2: "))
 diametro_p2 = int(input("ingrese el diametro (en km) del planeta 2: "))
 distancia_media_sol_p2 = int(input("ingrese la distancia media al sol (en km) del planeta 2: "))
+periodo_orbital_p2 = float(input("Ingrese el periodo orbital (en años) del planeta 2: "))
+periodo_rotacion_p2 = float(input("Ingrese el periodo de rotación (en dias) del planeta : "))
 
-Planeta1 = Planeta(nombre_p1,cantidad_satelites_p1,masa_p1,volumen_p1,diametro_p1,distancia_media_sol_p1,Tipo_de_planeta.Terrestre,True)
-Planeta2 = Planeta(nombre_p2,cantidad_satelites_p2,masa_p2,volumen_p2,diametro_p2,distancia_media_sol_p2,Tipo_de_planeta.Enano,False)
+Planeta1 = Planeta(nombre_p1,cantidad_satelites_p1,masa_p1,volumen_p1,diametro_p1,distancia_media_sol_p1,Tipo_de_planeta.Terrestre,True, periodo_orbital_p1, periodo_rotacion_p1)
+Planeta2 = Planeta(nombre_p2,cantidad_satelites_p2,masa_p2,volumen_p2,diametro_p2,distancia_media_sol_p2,Tipo_de_planeta.Enano,False, periodo_orbital_p2, periodo_rotacion_p2)
 
 print("Datos planeta 1:")
 Planeta1.mostrar_datos_planeta()
